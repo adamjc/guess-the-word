91dc6c38aab8356c9f4d819607e5b8f63f5939f1
adamjc <adamjc86@gmail.com>
2022-01-13 21:19:21 +0000

    Handle input

--- src/app.js
+++ src/app.js
@@ -2,5 +2,44 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 
 const { Letters } = require('./letters.js')
+import pickWord from './words.js'
 
-ReactDOM.render(<Letters />, document.getElementById('app'))
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      word: pickWord(),
+      letters: [],
+      currentLine: 1
+    }
+
+    console.log(`word is ${this.state.word}`)
+  }
+
+  handleCharacterChange = ({order, letters}) => {
+    console.log(this.state)
+    this.setState(letters[order] = letters)
+  }
+
+  handleClick = () => {
+    console.log(this.state)
+    if (this.state.word === this.state.letters.join("")) {
+      console.log("congratulations")
+    } else {
+      console.log(`you guessed ${this.state.letters.join("")}`)
+    }
+
+    this.setState(state => ({currentLine: state.currentLine + 1}))
+  }
+
+  render () {
+    return (
+      <div>
+        <Letters order="1" onCharacterChange={this.handleCharacterChange} disabled={this.state.currentLine === 1 ? false : true}/>
+        <button onClick={this.handleClick}>Go</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- src/letters.js
+++ src/letters.js
@@ -1,18 +1,35 @@
+// TODO: How to make each letter uniquely identifiable re: cssClasses ?
+
 import React from 'react'
 
 export class Letters extends React.Component {
   constructor (props) {
     super(props)
+    this.state = { order: this.props.order, letters: [] }
+  }
+
+  handleChange = (e) => {
+    if (!e.target.value.match(/[a-zA-Z]/)) {
+      e.target.value = ''
+      return
+    }
+
+    const value = e.target.value.toUpperCase()
+    const index = e.target.attributes.data.value
+    this.setState(state => state.letters[index] = value)
+    e.target.value = value
+    
+    this.props.onCharacterChange(this.state)
   }
 
   render () {
     return (
-      <div class="letters">
-        <input type="text" className="character" minlength="1" maxlength="1" id="char-1"/>
-        <input type="text" className="character" minlength="1" maxlength="1" id="char-2"/>
-        <input type="text" className="character" minlength="1" maxlength="1" id="char-3"/>
-        <input type="text" className="character" minlength="1" maxlength="1" id="char-4"/>
-        <input type="text" className="character" minlength="1" maxlength="1" id="char-5"/>
+      <div className="letters">
+        <input type="text" disabled={this.props.disabled} className="character" data="1" minLength="1" maxLength="1" onChange={this.handleChange}/>
+        <input type="text" disabled={this.props.disabled} className="character" data="2" minLength="1" maxLength="1" onChange={this.handleChange}/>
+        <input type="text" disabled={this.props.disabled} className="character" data="3" minLength="1" maxLength="1" onChange={this.handleChange}/>
+        <input type="text" disabled={this.props.disabled} className="character" data="4" minLength="1" maxLength="1" onChange={this.handleChange}/>
+        <input type="text" disabled={this.props.disabled} className="character" data="5" minLength="1" maxLength="1" onChange={this.handleChange}/>
       </div>
     )
   }
